7fc3692f65751f3db09ee7f721bd42bce1d531ec
cymon <cymon@volkov>
2019-08-17 13:58:49 +0200

    better paths

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+WORKDIR=$HOME/works/xaya
 docker rm gambit;
 docker run -d --name gambit \
-           -v=/home/cymon/works/xaya/projects:/opt/projects \
-           -v=/home/cymon/works/xaya/xaya-data:/var/xaya \
-           -v=/home/cymon/works/xaya/docker/xaya.conf:/root/.xaya/xaya.conf \
+           -v=$WORKDIR/projects:/opt/projects \
+           -v=$WORKDIR/xaya-data:/var/xaya \
+           -v=$WORKDIR/docker/xaya.conf:/root/.xaya/xaya.conf \
            xaya
 
